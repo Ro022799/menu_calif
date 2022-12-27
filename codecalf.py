59ce1afab3be77c006c37fe0ca3f452dc45df117
Ro022799 <120543710+Ro022799@users.noreply.github.com>
2022-12-26 20:49:59 -0600

    Main variables.
    Initializing the main variables for the program like 'nombres', 'apellidos', 'calificaciones', 'materias'.

---
+++ codecalf.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env pyhton
+nombres = 'Maria,Luis,Rodrigo,Monica,Ruben,Gaby,Irma,Ofelia'.split(',')
+apellidos = 'Fernandez,Carmona,Marquez,Santana,Lopez,Nunez,Ortega,Mancera'.split(',')
+print(nombre)
+print(apellidos)
