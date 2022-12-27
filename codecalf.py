824b833439361420fba885a8c2a92c75bb013f5c
Your Name <your@example.com>
2022-12-26 21:17:10 -0600

    Add a function tha contains all the main variables that contains info we'll use later.

--- codecalf.py
+++ codecalf.py
@@ -1,5 +1,8 @@
-#!/usr/bin/env pyhton
-nombres = 'Maria,Luis,Rodrigo,Monica,Ruben,Gaby,Irma,Ofelia'.split(',')
-apellidos = 'Fernandez,Carmona,Marquez,Santana,Lopez,Nunez,Ortega,Mancera'.split(',')
-print(nombre)
-print(apellidos)
+#!/usr/bin/env/ python
+def variables_iniciales():
+	nombres = 'Maria,Luis,Rodrigo,Monica,Ruben,Gaby,Irma,Ofelia'.split(',')
+	apellidos = 'Fernandez,Carmona,Marquez,Santana,Lopez,Nunez,Ortega,Mancera'.split(',')
+	materias = 'Fisica,Espanol,Matematicas,Quimica,Geografia'.split(',')
+	puntajes_max =[20,20,30,50,50]
+	return nombres, apellidos, materias, puntajes_max
+
